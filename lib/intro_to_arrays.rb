22cffed390f55c05c22613d30124c857a1b5f3a3
Jeffrey Paulino <jeffreypaulino@gmail.com>
2019-03-25 14:38:11 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -7,6 +7,10 @@ def array_with_two_elements
   new_array = [0,1]
 end
 
-def first_element
-  new_array.push("First element")
+def first_element(firstIndex)
+  new_array[firstIndex]
+end
+
+def third_element()
+  array[2]
 end
